1f220ca4449cd479c519f68bd14d7d0eb2d4b6ac
Nikita SHeremeta <nsheremeta@systematica-consulting.ru>
2021-11-14 23:07:45 +0300

    Added auth middleware

--- src/api/v1/routes/index.js
+++ src/api/v1/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { body } = require('express-validator');
 const userController = require('../controllers/user-contoller');
+// const authMiddleware = require('../middlewares/auth-middleware');
 
 const router = express.Router();
 
